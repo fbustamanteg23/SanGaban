1b95368952f5a825d2a947002a5a60313c78493f
Fernando Bustamante <fbustamanteg23@hotmail.com>
2023-06-23 15:42:22 -0500

    usuario_cambio_2

--- SanGaban_WebAPI/Modelos/Dto/UsuarioDto.cs
+++ SanGaban_WebAPI/Modelos/Dto/UsuarioDto.cs
@@ -7,16 +7,16 @@ namespace SanGaban_WebAPI.Modelos.Dto
         
 
         public int IdUsuario { get; set; }
-        public int registro { get; set; }
+        public String registro { get; set; }
 
         [Required]
         public String nombreCompleto { get; set; }
         public String apellido_paterno { get; set; }
         public String apellido_materno { get; set; }
         public String correo { get; set; }
-        public String IdRol { get; set; }
+        public int IdRol { get; set; }
         public String clave { get; set; }
-        public String esActivo { get; set; }
+        public Boolean esActivo { get; set; }
 
         public DateTime fechaRegistro { get; set; }
     }
